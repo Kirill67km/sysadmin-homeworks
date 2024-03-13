460673f70fb2475842310a0a86e6cadfcda03d8a
galchonkov <109647074+Kirill67km@users.noreply.github.com>
2024-03-13 22:22:05 +0300

    Update dick_vm.tf

--- terraform/src_3/dick_vm.tf
+++ terraform/src_3/dick_vm.tf
@@ -1,34 +1,33 @@
 resource "yandex_compute_disk" "storage_disk" {
   count = 3
   name       = "storage-disk-${tostring(count.index + 1)}"
-  type       = "network-hdd"
+  type       = var.vms_defset.disktype
   zone       = "ru-central1-a"
   size       = 1
 }
 
 resource "yandex_compute_instance" "storage" {  
   name        = "storage"
-  platform_id = "standard-v1"
+  platform_id = var.vms_defset.platform_id
    
   dynamic "secondary_disk" {
-    for_each = { for s in yandex_compute_disk.storage_disk[*]: s.name=> s }
+    for_each = { for s in yandex_compute_disk.storage_disk : s.name => s }
     content {
         disk_id=secondary_disk.value.id
     }
   }
-   
 
   resources {
-    cores  = 2
-    memory =  1
+    cores  = var.vms_defset.resources.cpu
+    memory =  var.vms_defset.resources.ram
     core_fraction = 20 
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id
-      type =  "network-hdd"
-      size = 5
+      type =  var.vms_defset.disktype
+      size =  var.vms_defset.disksize
     }   
   }
 
