44afc09c1cb76a616b17c41b160474e9256913e9
galchonkov <109647074+Kirill67km@users.noreply.github.com>
2024-03-13 22:21:42 +0300

    Update count-vm.tf

--- terraform/src_3/count-vm.tf
+++ terraform/src_3/count-vm.tf
@@ -2,19 +2,19 @@ resource "yandex_compute_instance" "web" {
   count = 2
 
   name        = "netology-develop-platform-web-${count.index + 1}"
-  platform_id = "standard-v1"
+  platform_id = var.vms_defset.platform_id
 
   resources {
-    cores         = 2
-    memory        = 1
+    cores         = var.vms_defset.resources.cpu
+    memory        = var.vms_defset.resources.cpu
     core_fraction = 20
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
-      type     = "network-hdd"
-      size     = 5
+      type     = var.vms_defset.disktype
+      size     = var.vms_defset.disktype
     }
   }
 
@@ -27,6 +27,8 @@ resource "yandex_compute_instance" "web" {
   network_interface {
     subnet_id = yandex_vpc_subnet.netology.id
     nat       = true
+
+    security_group_ids = [yandex_vpc_security_group.example.id]
   }
   allow_stopping_for_update = true
 }
