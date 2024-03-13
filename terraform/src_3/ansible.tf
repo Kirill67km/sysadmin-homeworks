b3507d5a89d29f8b7e4a842dd2d14df2cd106118
galchonkov <109647074+Kirill67km@users.noreply.github.com>
2024-03-13 22:21:20 +0300

    Update ansible.tf

--- terraform/src_3/ansible.tf
+++ terraform/src_3/ansible.tf
@@ -3,6 +3,6 @@ resource  "local_file" "inventoryfile" {
      content= templatefile("${abspath(path.module)}/ansible/hosts.tftpl",{
         webservers= [for i in yandex_compute_instance.web: i ] 
         databases=  [for k,v in yandex_compute_instance.db: v ] 
-        storages= tolist( [yandex_compute_instance.storage])  
+        storages=   [yandex_compute_instance.storage]   
      })  
  }
